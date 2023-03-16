c9a0c1d45ee4eac2ff5bc4910b14d759f3d8077b
therandomspoon <107148755+therandomspoon@users.noreply.github.com>
2023-03-16 12:55:51 +0000

    Create

---
+++ script.js
@@ -0,0 +1,11 @@
+function checkTimeAndPlay() {
+  var date = new Date();
+  // Check if time is 16:12:54
+  if (date.getHours() == 13 && date.getMinutes() == 07 && date.getSeconds() == 22)
+    document.getElementById('Breaking Bad.mp4').play();
+  else {
+    console.log("Time isn't match");
+    setTimeout(checkTimeAndPlay, 1000);
+  }
+}
+checkTimeAndPlay();
